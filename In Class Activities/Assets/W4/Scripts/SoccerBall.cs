e98f6834f9a3cb10b6c633b7af7ad49bd75d14ca
minjoos5 <minjoos5@uci.edu>
2025-10-20 18:48:06 -0700

    step 4 done

--- In Class Activities/Assets/W4/Scripts/SoccerBall.cs
+++ In Class Activities/Assets/W4/Scripts/SoccerBall.cs
@@ -21,10 +21,10 @@ public class SoccerBall : MonoBehaviour
     //
     // Also, uncomment and FIX the Debug.Log line.
 
-    private void OnTriggerEnter (Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         // finish STEP 1 by uncommenting and fixing the below line!
-        
+
 
         // STEP 2 -------------------------------------------------------------
         // Write an IF STATEMENT to check if the game object we collided with
@@ -37,6 +37,7 @@ public class SoccerBall : MonoBehaviour
         if (other.gameObject.tag.Equals("Goal"))
         {
             Debug.Log("Goal");
+            MadeGoal();
         }
 
         // STEP 2 -------------------------------------------------------------
@@ -49,21 +50,30 @@ public class SoccerBall : MonoBehaviour
     // Next, we're going to make a method named MadeGoal to call if the
     //      SoccerBall collided with an object tagged "Goal".
     // MadeGoal RETURNS NOTHING, and has NO INPUT.
+
+    private void MadeGoal()
+    {
+        Debug.Log("Goal");
+        _goalVFX.Play();
+    }
+
+
+
     // 
     // Once you've created MadeGoal,
     //      move your Debug.Log() statement into MadeGoal, and
     //      call MadeGoal from inside your if statement in OnTriggerEnter.
-    
-        // STEP 4 -------------------------------------------------------------
-        // _goalVFX is a ParticleSystem, a Component for creating VFX.
-        // ParticleSystem has a method named Play() that displays the VFX:
-        // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
-        //
-        // Call Play on _goalVFX.
 
-        // STEP 4 -------------------------------------------------------------
-    
-    
+    // STEP 4 -------------------------------------------------------------
+    // _goalVFX is a ParticleSystem, a Component for creating VFX.
+    // ParticleSystem has a method named Play() that displays the VFX:
+    // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
+    //
+    // Call Play on _goalVFX.
+
+    // STEP 4 -------------------------------------------------------------
+
+
     // STEP 3 -----------------------------------------------------------------
 
 
@@ -82,7 +92,7 @@ public class SoccerBall : MonoBehaviour
     //      2. Use your MadeGoal method to update the points and text.
     //
     // STEP 5 -----------------------------------------------------------------
-    
+
 
     // STEP 6 -----------------------------------------------------------------
     // Like the last step, these flags do NOT show you where to put all of the
@@ -99,4 +109,5 @@ public class SoccerBall : MonoBehaviour
     //      2. Use the MadeGoal method to reset your
     //              time variable when the player makes a goal. 
     // STEP 6 -----------------------------------------------------------------
+
 }
